900adf5cd3397c5a76395cdea5eac1d03a0b4806
cdascientist <cdascientist@icloud.com>
2025-04-01 17:03:15 -0600

    Add Google OAuth integration to App
    Updated `package-lock.json` to include `@react-oauth/google` package version `0.12.1` and removed its license information. Imported `GoogleOAuthProvider` in `App.jsx` and added a constant `googleClientId` for the Google Client ID. Wrapped `BrowserRouter` with `GoogleOAuthProvider` using the provided client ID. Removed the `div` element that previously wrapped the `BrowserRouter` component.

--- mw.client/src/App.jsx
+++ mw.client/src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import Home from './pages/Home';
 import About from './pages/About';
 import LiveChat from './pages/LiveChat';
 import './App.css';
 
 function App() {
+    // Replace with your actual Google Client ID from Google Cloud Console
+    const googleClientId = 'YOUR_GOOGLE_CLIENT_ID';
+
     return (
-        <div>
+        <GoogleOAuthProvider clientId={"7074654684-866fnk2dp7c23e54nt35o5o3uvlm6fbl.apps.googleusercontent.com"}>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
@@ -14,7 +18,7 @@ function App() {
                     <Route path="/chat" element={<LiveChat />} />
                 </Routes>
             </BrowserRouter>
-        </div>
+        </GoogleOAuthProvider>
     );
 }
 
