5c9f60f2a6ba6636dedf59b1047db0c86e10a606
cdascientist <cdascientist@icloud.com>
2025-04-13 16:07:39 -0600

    Add new models and update database context management
    - Updated `MW.Server.csproj` to include new package references and a `Models` folder.
    - Created `Client.cs`, `SearchResult.cs`, `WorkdayStepOne.cs`, and `WorkdayStepOneJob.cs` with relevant properties and data annotations.
    - Removed `MWDbContext.cs` and introduced `DefaultdbContext.cs` for improved database context management.
    - Added new React component `Analysis_Home.jsx` for user authentication and UI rendering.

--- MW.Server/Models/Client.cs
+++ MW.Server/Models/Client.cs
@@ -1,27 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace MW.Server.Models;
 
+[Table("client")]
 public partial class Client
 {
+    [Key]
+    [Column("id")]
     public int Id { get; set; }
 
+    [Column("my_id")]
     public int? MyId { get; set; }
 
+    [Column("first_name")]
+    [StringLength(100)]
     public string? FirstName { get; set; }
 
+    [Column("last_name")]
+    [StringLength(100)]
     public string? LastName { get; set; }
 
+    [Column("phone")]
+    [StringLength(100)]
     public string? Phone { get; set; }
 
+    [Column("email")]
+    [StringLength(100)]
     public string? Email { get; set; }
 
+    [Column("slackchannelid")]
+    [StringLength(100)]
     public string? Slackchannelid { get; set; }
 
+    [Column("slackchannel")]
+    [StringLength(100)]
     public string? Slackchannel { get; set; }
 
+    [Column("current_job_id")]
     public int? CurrentJobId { get; set; }
 
+    [Column("current_job_chat")]
     public int? CurrentJobChat { get; set; }
+
+    [Column("ext_date")]
+    [StringLength(255)]
+    public string? ExtDate { get; set; }
+
+    [Column("ext2_date")]
+    [StringLength(255)]
+    public string? Ext2Date { get; set; }
 }
